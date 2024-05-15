45ed06519a8e84acc8bc4d736620060ce4eed687
Makayla K. Rodriguez <makaylak.rodriguez@Makaylas-Laptop.local>
2024-05-14 17:26:11 -0700

    Added header and header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import { useState } from 'react'
 import './App.css'
+import SearchComponent from './Components/SearchComponent';
+import SortComponent from './Components/Sort';
+
+// without starter code npm create vite@latest my-vite-app --template react, with starter code it is already instantiated
+// npm install same layer as project
 
 const App = () => {
-  <div className="App">
-    
-  </div>
+    return (
+      <>
+        <header className='App-header'>
+          <div className='title'>
+            <img src="/movie.png"></img>
+            <h1>Flixster</h1>
+            <img src="/movie.png"></img>
+          </div>
+          <div className='filters'>
+            <SearchComponent />
+            <SortComponent />
+          </div>
+        </header>
+      </>
+  );    
 }
 
 export default App
+
+
+// function App() {
+//   // Logic and state hooks will be placed here
+
+//   return (
+//     <div className="App">
+
+//       {/* Child components will be rendered here */}
+  
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/Components/Sort.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import '../ComponentsCSS/Sort.css'
+
+
+const SortComponent = ({ onChange }) =>  {
+  return (
+    // JSX code for rendering the component
+      <form className="sort" onChange={onChange} defaultValue="">
+          <select id="cars" name="cars">
+            <option value="" disabled selected>Sort By</option>
+            <option value="popularity.desc">Most Popular</option>
+            <option value="release_date.desc">Recent Release Date</option>
+            <option value="vote_average.desc">Highest Rating</option>
+        </select>   
+      </form>
+  );
+}
+
+export default SortComponent;
